232ee58c41d8703f5ddaf6f7064c0c9921afbd7f
angello <angellovillanuevaind@gmail.com>
2021-05-01 19:26:16 -0400

    Reto de blackjack ciclico de Hackerrank.com

---
+++ blackjack.py
@@ -0,0 +1,32 @@
+def run():
+    a = []
+    while True:
+        cj = (input(''))
+        if cj != "STOP":
+            a.append(int(cj))
+        else:
+            break
+    suma_cj = sum(a)
+
+
+    b = []
+    cant_cartas = int(input(''))
+    for i in range(cant_cartas):
+        entrada = int(input(''))
+        b.append(entrada)
+    suma_cm = sum(b)
+    
+    if suma_cj == 21:
+        print('Gana el jugador')
+    elif suma_cj < 21:
+        if suma_cj > suma_cm:
+            print('Gana el jugador')
+        elif suma_cm > 21:
+            print('Gana el jugador')
+        else:
+            print('Gana la mesa')
+    elif suma_cj > 21:
+        print('Gana la mesa')
+
+if __name__=='__main__':
+    run()
