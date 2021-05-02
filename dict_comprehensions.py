a24c7002b337580add58fa035992a044926801d5
angello <angellovillanuevaind@gmail.com>
2021-05-02 11:42:38 -0400

    Cambio en el dictionary comprehension

--- dict_comprehensions.py
+++ dict_comprehensions.py
@@ -7,8 +7,7 @@ def run():
 
     # my_dict = {i: i**3 for i in range(1,101) if i % 3 != 0}
     
-    my_dict = {i: i**0.5 for i in range(1,101)}
-
+    my_dict = {i: i**2 for i in range(100) if i % 4 == 0}
     print(my_dict)
 
 
