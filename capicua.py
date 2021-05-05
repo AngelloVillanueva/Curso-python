253a2f1d3a3d83138bd2ec0195223a4992711e74
angello <angellovillanuevaind@gmail.com>
2021-05-05 15:44:57 -0400

    Mejora de reto hacker rank

--- capicua.py
+++ capicua.py
@@ -1,16 +1,26 @@
-def es_capicua(a):
-    
-    if a == a[::-1]:
-        print('Es cap')
-        return True
-    elif a != a[::-1]:
-        a = str(int(a)*2)
-        print(a)
-        es_capicua(a)
-   #b = a[::-1]
+# def es_capicua(a):
+#     if a == a[::-1]:
+#         return True
+
+
+# def calcular_capicua(a):
+#     if a != a[::-1]:
+#         b = int(a) + int(a[::-1])
+#         calcular_capicua(str(b))
+#     elif a == a[::-1]:
+#         print(a)
+
+
+def encontrar_capicua(a):
+    if a != a[::-1]:
+        b = abs(int(a[::-1]) - int(a)))
+        print(b)
+        encontrar_capicua(str(b))
+        
+
 
 if __name__=='__main__':
     a = input('Engresa un numero: ')
     es_capicua(a)
-
-    
+    # calcular_capicua(a)
+    # encontrar_capicua(a)
